70861160bdde9ae5942ecb3bf8f0d03d4d7269ad
kuanyilee85 <kuanyilee85@gmail.com>
2020-12-09 23:39:29 -0500

    environment setup

---
+++ src/main/java/com/kennycode/RestfulwebApplication.java
@@ -0,0 +1,13 @@
+package com.kennycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kennycode/RestfulwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kennycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
