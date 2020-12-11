cad8dcf0e972fce6cf5166bf58bfec89598ecbab
kuanyilee85 <kuanyilee85@gmail.com>
2020-12-10 23:18:25 -0500

    CrossOrigin

--- src/main/java/com/kennycode/HelloWorldController.java
+++ src/main/java/com/kennycode/HelloWorldController.java
@@ -1,11 +1,13 @@
 package com.kennycode;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-// declare Controller
+// Controller
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class HelloWorldController {
 
     // GET
