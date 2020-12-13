2e2736d098c66e74d5354e5b7adb1960c496919c
kuanyilee85 <kuanyilee85@gmail.com>
2020-12-12 23:40:12 -0500

    handle API error

--- src/main/java/com/kennycode/HelloWorldController.java
+++ src/main/java/com/kennycode/HelloWorldController.java
@@ -22,11 +22,12 @@ public class HelloWorldController {
 
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
-        return new HelloWorldBean("Hello World");
+        return new HelloWorldBean("Hello World Bean");
     }
 
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello World, %s",name));
+//        throw new RuntimeException("Something wrong");
     }
 }
